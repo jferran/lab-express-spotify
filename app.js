3e6a5b4b1246941feca0857e0601688d4c499e8d
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-05-07 23:13:51 +0200

    finished without styling

--- app.js
+++ app.js
@@ -27,3 +27,46 @@ const spotifyApi = new SpotifyWebApi({
 // Our routes go here:
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+app.get('/', (req, res, next) => res.render("index"))
+app.get('/artist-search', (req, res, next) =>{
+    //const { artist } = req.query
+    let artist = req.query.artist
+    
+    console.log("searching", artist)
+    
+    spotifyApi
+    .searchArtists(artist)
+    .then(data => {
+        //console.log('The received data from the API: ', JSON.stringify(data.body));
+        // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+        //res.render("")
+        res.render("artist-search-results", {artists: data.body.artists.items})
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+    
+})
+
+app.get('/albums/:artistId', (req, res, next) => {
+    // .getArtistAlbums() code goes here
+    const { artistId } = req.params
+    spotifyApi.getArtistAlbums(artistId)
+    .then(data => {
+        //console.log("The retrieved artists albums data: ", JSON.stringify(data))
+        res.render("albums", {albums: data.body.items})
+    })
+    .catch(err => console.log("Error ocurred while retrieving artist albums: ", err))
+
+  });
+
+  app.get('/tracks/:albumId', (req, res, next) => {
+    // .getArtistAlbums() code goes here
+    const { albumId } = req.params
+    spotifyApi.getAlbumTracks(albumId)
+    .then(data => {
+        console.log("The retrieved tracks data: ", JSON.stringify(data))
+        res.render("tracks", {tracks: data.body.items})
+    })
+    .catch(err => console.log("Error ocurred while retrieving artist albums: ", err))
+
+  });
